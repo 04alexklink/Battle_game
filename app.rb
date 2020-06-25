e839e715d3e79c925954a76aae58bb2146563ee9
Makers Admin <makersadmin@Makerss-MacBook-Air.local>
2020-06-25 15:06:09 +0100

    Sessions activated to store paramater variables

--- app.rb
+++ app.rb
@@ -1,13 +1,20 @@
 require 'sinatra/base'
 
 class Battle < Sinatra::Base
+  enable :sessions
   get '/' do
     erb:index
   end
   post '/names' do
-   @First_Player_Name = params[:First_Player_Name]
-   @Second_Player_Name = params[:Second_Player_Name]
-   erb:players
+    session[:First_Player_Name] = params[:First_Player_Name]
+    session[:Second_Player_Name] = params[:Second_Player_Name]
+    redirect '/players'
+  end
+
+  get '/players' do
+    @First_Player_Name = session[:First_Player_Name]
+    @Second_Player_Name = session[:Second_Player_Name]
+    erb:players
   end
   run! if app_file == $0
 end 
