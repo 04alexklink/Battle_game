6a186a4f7662617ef0090fd0bd6f2f3161b52278
Stuart <scpitc@gmail.com>
2020-06-25 15:30:22 +0100

    Added Payer 2 HP display feature test and passing test

--- spec/features/features_spec.rb
+++ spec/features/features_spec.rb
@@ -4,6 +4,8 @@
 #       expect(page).to have_content 'Testing infrastructure working!'
 #     end
 #   end
+
+
   feature 'enter player names' do
     scenario 'can enter player names and get a page returned with names included in text' do
       visit('/')
@@ -13,3 +15,13 @@
       expect(page).to have_content("Alex v Adam")
       end 
   end 
+
+  feature 'display player hit points' do
+    scenario 'Player 2 Hit Points displayed to Player 1' do
+      visit('/')
+      fill_in :First_Player_Name, :with => "Alex"
+      fill_in :Second_Player_Name, :with => "Adam"
+      click_button "Submit"
+      expect(page).to have_content("Player 2 HP: 60")
+    end
+  end
